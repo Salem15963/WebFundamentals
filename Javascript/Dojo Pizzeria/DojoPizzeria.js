0d00b955e1f364345f7810b05cd8fce612862399
Salem15963 <Omar.salemps@gmail.com>
2022-08-30 14:13:32 +0300

    commit

---
+++ Javascript/Dojo Pizzeria/DojoPizzeria.js
@@ -0,0 +1,29 @@
+var pizza = {
+    crustType:   ["deep dish","hand tossed","stuffed Crust","cracker Crust"],
+    sauceType:  ["traditional", "marinara","white Garlic Sauce","buffalo Sauce"],
+    cheeses:   ["mozzarella","feta","swiss cheese","blue cheese"],
+    toppings:  ["pepperoni", "sausage" ,"mushrooms", "olives", "onions","chicken Cutlet","spinach"]
+};
+    
+console.log(pizza);
+
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var p1 = pizzaOven("deep dish","traditional",["mozzarella"],["pepperoni","sausage"]);
+console.log(p1);
+
+var p2 = pizzaOven("hand tossed","marinara","mozzarella","feta",["mushrooms", "olives", "onions"]);
+console.log(p2);
+
+var p3 = pizzaOven("stuffed Crust","white Garlic Sauce","swiss cheese","chicken Cutlet");
+console.log(p3);
+
+var p4 = pizzaOven("Cracker Crust","Buffalo Sauce","blue cheese","Spinach");
+console.log(p4);
