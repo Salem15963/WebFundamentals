632c828b8ceb55a767727ba9750da0daf42a55b6
Salem15963 <Omar.salemps@gmail.com>
2022-08-30 15:45:19 +0300

    commit

--- Css/Loop Challenges/looptest.js
+++ Css/Loop Challenges/looptest.js
@@ -3,34 +3,27 @@ if (i%2 !== 0) {
     console.log(i)
 }
 
-for(var i=-3.5; i<=4; i+=1.5) {
-    console.log(i);
-}
+// for(var i=-3.5; i<=4; i+=1.5) {
+//     console.log(i);
+// }
 
-for(var i=100; i>=0; i-=3) {
-    console.log(i);
-}
+// for(var i=100; i>=0; i-=3) {
+//     console.log(i);
+// }
 
-for(var i=1; i<=5050; i++) {
-    console.log(i);
-}
+// var sum = 0;
+// for(var x=1 ; x<=100; x++)
+// sum=sum+x
+// console.log(sum)
 
-for(var i=12 ; i<479001600; i--) {
+// for(var i=1 ; i<12; i--) {
     
-    console.log(i);
-}
+//     console.log(i);
+// }
 
-function factorial(n){
-    let answer = 1;
-    if (n == 0 || n == 1){
-    return answer;
-    }else{
-      for(var i = n; i >= 1; i--){
-        answer = answer * i;
-      }
-      return answer;
-    }  
-  }
-  let n = 12;
-  answer = factorial(n)
-  console.log("The factorial of " + n + " is " + answer);
+// var product=1;
+
+// for (var i=1; i<=12; i++){
+//     product*=i;
+// }
+// console.log(product)
