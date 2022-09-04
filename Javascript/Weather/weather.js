7e8ec062244b39130f71a76f9612fc893049c773
Salem15963 <Omar.salemps@gmail.com>
2022-09-05 00:29:26 +0300

    Weather code

---
+++ Javascript/Weather/weather.js
@@ -0,0 +1,31 @@
+
+function myFunction() {
+    alert("Loading weather report...");
+}
+
+function hideMe(hide){
+var element=hide.parentElement;
+element.remove()
+}
+
+    function cToF(celsius) 
+    {
+var cTemp = celsius;
+      var cToFahr = cTemp * 9 / 5 + 32;
+
+    }
+    
+    function fToC(fahrenheit) 
+    {
+var fTemp = fahrenheit;
+      var fToCel = (fTemp - 32) * 5 / 9;
+    } 
+
+    Function toC(){
+
+        if (measureunit.)
+    }
+
+    
+
+
