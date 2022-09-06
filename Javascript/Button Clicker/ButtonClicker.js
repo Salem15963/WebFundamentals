910ab3293b44ab122a1d5e74e6156262ee96ce33
Salem15963 <Omar.salemps@gmail.com>
2022-09-06 13:18:30 +0300

    button clicker commit

---
+++ Javascript/Button Clicker/ButtonClicker.js
@@ -0,0 +1,12 @@
+
+function doSomething() {
+  alert("Ninja was liked");
+}
+
+function DisplayInput(){
+document.querySelector(".btn0").innerHTML = "logout";
+}
+
+function remove(element) {
+  element.remove();
+}
