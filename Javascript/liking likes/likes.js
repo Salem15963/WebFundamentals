8110baed0db4a6286bb3993e9fdc782578b28c11
Salem15963 <Omar.salemps@gmail.com>
2022-09-07 16:27:11 +0300

    liking likes commit

---
+++ Javascript/liking likes/likes.js
@@ -0,0 +1,26 @@
+var element=document.querySelector(".number");
+var counter=9;
+
+function fun(){
+   counter = counter + 1;
+   element.innerText = counter;
+
+}
+
+var element1=document.querySelector(".number-1");
+var counter1=12;
+
+function funny(){
+   counter1 = counter1 + 1;
+   element1.innerText = counter1;
+
+}
+
+var element2=document.querySelector(".number-2");
+var counter2=9;
+
+function funnie(){
+   counter2 = counter2 + 1;
+   element2.innerText = counter2;
+
+}
